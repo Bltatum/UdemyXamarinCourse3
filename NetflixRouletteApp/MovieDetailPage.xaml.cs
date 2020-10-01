6813c00c860ba78fae919480f8b815b76e771fe4
brennen <bmar566@gmail.com>
2020-10-01 12:57:40 -0500

    Completed Movies App exercise

---
+++ NetflixRouletteApp/MovieDetailPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using NetflixRouletteApp.Models;
+using NetflixRouletteApp.Services;
+using Xamarin.Forms;
+
+namespace NetflixRouletteApp
+{ 
+		public partial class MovieDetailPage : ContentPage
+		{
+			private MovieService _movieService = new MovieService();
+			private Movie _movie;
+
+			public MovieDetailPage(Movie movie)
+			{
+				if (movie == null)
+					throw new ArgumentNullException(nameof(movie));
+
+				_movie = movie;
+
+				InitializeComponent();
+			}
+
+			protected override async void OnAppearing()
+			{
+				BindingContext = await _movieService.GetMovie(_movie.ImdbID);
+
+				base.OnAppearing();
+			}
+		}
+	
+}
---
+++ NetflixRouletteApp/Services/MovieService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NetflixRouletteApp.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NetflixRouletteApp.Services
+{
+    public class MovieService
+    {
+		public static readonly int MinSearchLength = 5;
+
+		private const string Url = "https://www.omdbapi.com/?i=tt3896198&apikey=8e15c5b7&";
+		private const string UrlTitle = "https://www.omdbapi.com/?apikey=8e15c5b7&";
+
+
+		private HttpClient _client = new HttpClient();
+
+		public async Task<IEnumerable<Movie>> FindMoviesByActor(string title)
+		{
+			if (title.Length < MinSearchLength)
+				return Enumerable.Empty<Movie>();
+
+			var response = await _client.GetAsync($"{Url}s={title}");
+
+			if (response.StatusCode == HttpStatusCode.NotFound)
+				return Enumerable.Empty<Movie>();
+
+			var content = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<List<Movie>>(JObject.Parse(content)["Search"].ToString());
+			
+		}
+
+		public async Task<Movie> GetMovie(string imdbId)
+		{
+			var response = await _client.GetAsync($"{UrlTitle}i={imdbId}");
+
+			if (response.StatusCode == HttpStatusCode.NotFound)
+				return null;
+
+			var content = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<Movie>(content);
+		}
+	}
+}
+
+
+
